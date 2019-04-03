1f50d1413d2450f51057256b32d673622899c470
Wes-White <wesleyrwhite86@gmail.com>
2019-04-02 18:38:09 -0700

    New Binding

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit Binding is when this is what is left in the dot. ("this.name")
+* 2. Explicit Binding of this occurs when .call(), .apply(), or .bind() are used on a function.
+* 3. Window Binding of this is the global context whenever a function is invoke. (a global object)
+* 4. New Binding
 *
 * write out a code example of each explanation above
 */
@@ -28,11 +28,24 @@ const wes = {
 }
 
 console.log(wes.speak())
-
+ 
 
 // Principle 3
 
 // code example for New Binding
+
+let sayHello = function () {
+    console.log('Hello my name is ' + this.name)
+  }
+
+  let wes = {
+    name: 'Wes'
+  }
+
+ let intro = sayHello.bind(wes)
+  
+ intro()
+
 
 // Principle 4
 
